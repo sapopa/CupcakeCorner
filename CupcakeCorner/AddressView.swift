f46d18977465c5a8764040311c713ee550fb3f13
sebastian.popa <sebastian.popa@softvision.com>
2023-08-15 17:35:13 +0300

    Project 10 implementation

---
+++ CupcakeCorner/AddressView.swift
@@ -0,0 +1,43 @@
+//
+//  AddressView.swift
+//  CupcakeCorner
+//
+//  Created by sebastian.popa on 8/14/23.
+//
+
+import SwiftUI
+
+struct AddressView: View {
+    
+    @ObservedObject var order: Order
+    
+    var body: some View {
+        Form {
+            Section {
+                TextField("Name", text: $order.name)
+                TextField("Street address", text: $order.streetAddress)
+                TextField("City", text: $order.city)
+                TextField("Zip", text: $order.zip)
+            }
+            
+            Section {
+                NavigationLink {
+                    CheckoutView(order: order)
+                } label: {
+                    Text("Checkout")
+                }
+            }
+            .disabled(order.hasValidAddress == false)
+        }
+        .navigationTitle("Delivery details")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct AddressView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationStack {
+            AddressView(order: Order())
+        }
+    }
+}
